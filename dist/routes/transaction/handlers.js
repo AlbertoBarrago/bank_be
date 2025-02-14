ec5aedc20b90a77e8b03fdb6dca535954fdd0c56
Alberto Barrago <albertobarrago@gmail.com>
2025-02-14 14:39:52 +0100

    add all

---
+++ dist/routes/transaction/handlers.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.TransactionHandlers = void 0;
+const transaction_service_1 = require("../../services/transaction.service");
+class TransactionHandlers {
+    constructor(app) {
+        this.app = app;
+        this.transactionsService = new transaction_service_1.TransactionService(app);
+    }
+    async createTransaction(request, reply) {
+        const transaction = await this.transactionsService.createTransaction({
+            type: request.body.type,
+            amount: request.body.amount,
+        });
+        return reply.status(201).send(transaction);
+    }
+}
+exports.TransactionHandlers = TransactionHandlers;

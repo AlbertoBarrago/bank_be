83738745f667a211b4535732eb14ffe69bad5e0a
Alberto Barrago <albertobarrago@gmail.com>
2025-02-15 20:16:19 +0100

    Removed migrations

--- prisma/migrations/20250214164341_added_relationship/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE `Transaction` ADD COLUMN `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3);
-
--- AddForeignKey
-ALTER TABLE `Account` ADD CONSTRAINT `Account_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `Transaction` ADD CONSTRAINT `Transaction_accountId_fkey` FOREIGN KEY (`accountId`) REFERENCES `Account`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
